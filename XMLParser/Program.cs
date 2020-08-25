d40cebe6f77789c12cd7067865ddba38f9ab27c4
Özgür Çebi <oz6nr@hotmail.com>
2020-08-25 13:54:49 +0300

    Data class logic added

--- XMLParser/Program.cs
+++ XMLParser/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XMLParser;
+using System.Collections;
 
 namespace ParseXML
 {
@@ -13,10 +15,17 @@ namespace ParseXML
         {
             String URLString = "https://www.tcmb.gov.tr/kurlar/today.xml";
             XmlTextReader reader = new XmlTextReader(URLString);
+            ArrayList CurrencyArrayList = new ArrayList();
 
             string date = null;
             bool skipping = true;
-            string tempNameValue = "";
+            string tempReaderName = "";
+            string tempName = "";
+            string tempUnit = "";
+            string tempForexBuying = "";
+            string tempForexSelling = "";
+            string tempBanknoteBuying = "";
+            string tempBanknoteSelling = "";
 
             while (reader.Read())
             {
@@ -30,19 +39,27 @@ namespace ParseXML
                             Console.WriteLine("Tarih: " + date);
                         }
 
-                        if(reader.Name == "CurrencyName")
+                        if(reader.Name == "Unit")
                         {
                             skipping = false;
-                            Console.Write("\n");
                         }
                         else if(reader.Name == "CrossRateUSD")
                         {
                             skipping = true;
+                            CurrencyArrayList.Add(new CurrencyData(tempUnit, tempName, tempForexBuying, tempForexSelling, tempBanknoteBuying, tempBanknoteSelling));
+                            //reset temp values
+                            tempReaderName = "";
+                            tempName = "";
+                            tempUnit = "";
+                            tempForexBuying = "";
+                            tempForexSelling = "";
+                            tempBanknoteBuying = "";
+                            tempBanknoteSelling = "";
                         }
 
                         if(!skipping)
                         {
-                            tempNameValue = reader.Name;
+                            tempReaderName = reader.Name;
                         }
                         break;
                     case XmlNodeType.Text: //Display the text in each element.
@@ -50,14 +67,41 @@ namespace ParseXML
                         {
                             if(!String.IsNullOrEmpty(reader.Value))
                             {
-                                tempNameValue = tempNameValue + ": " + reader.Value;
-                                Console.WriteLine(tempNameValue);
+                                if (tempReaderName == "Unit")
+                                {
+                                    tempUnit = reader.Value;
+                                }
+                                else if (tempReaderName == "Isim")
+                                {
+                                    tempName = reader.Value;
+                                }
+                                else if (tempReaderName == "ForexBuying")
+                                {
+                                    tempForexBuying = reader.Value;
+                                }
+                                else if (tempReaderName == "ForexSelling")
+                                {
+                                    tempForexSelling = reader.Value;
+                                }
+                                else if (tempReaderName == "BanknoteBuying")
+                                {
+                                    tempBanknoteBuying = reader.Value;
+                                }
+                                else if (tempReaderName == "BanknoteSelling")
+                                {
+                                    tempBanknoteSelling = reader.Value;
+                                }
                             }
                             
                         }
                         
                         break;
                 }
+            }
+            
+            foreach(CurrencyData cd in CurrencyArrayList)
+            {
+                cd.PrintCurrencyInfo();
             }
             
         }
