20baadb27de6e9c53a9aaa8aa535ae4a622b341c
Özgür Çebi <oz6nr@hotmail.com>
2020-08-25 13:53:05 +0300

    Currency data class added

---
+++ XMLParser/CurrencyData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLParser
+{
+    class CurrencyData
+    {
+        public string unit;
+        public string currencyName;
+        public string forexBuying;
+        public string forexSelling;
+        public string banknoteBuying;
+        public string banknoteSelling;
+
+        public CurrencyData(string u, string cn, string fb, string fs, string bb, string bs)
+        {
+            unit = u;
+            currencyName = cn;
+            forexBuying = fb;
+            forexSelling = fs;
+            banknoteBuying = bb;
+            banknoteSelling = bs;
+        }
+
+        public void PrintCurrencyInfo()
+        {
+            Console.WriteLine("Currency Name: " + currencyName + 
+                "\nUnit: " + unit + 
+                "\nForex Buying: " + forexBuying + 
+                "\nForex Selling: " + forexSelling + 
+                "\nBanknote Buying: " + banknoteBuying + 
+                "\nBanknote Selling: " + banknoteSelling + "\n");
+        }
+
+    }
+}
